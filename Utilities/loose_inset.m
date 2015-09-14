92e2e53122a90fb878beef92db1f515e8b561e43
rmcdermo <randy.mcdermott@gmail.com>
2015-09-14 15:34:24 -0400

    Extinction: add processing script for experimental data

---
+++ Utilities/loose_inset.m
@@ -0,0 +1,7 @@
+set(gca,'units','centimeters')
+pos = get(gca,'Position');
+li = get(gca,'LooseInset');
+set(gcf, 'PaperUnits','centimeters');
+set(gcf, 'PaperSize', [pos(3)+li(1)+li(3) pos(4)+li(2)+li(4)]);
+set(gcf, 'PaperPositionMode', 'manual');
+set(gcf, 'PaperPosition',[0 0 pos(3)+li(1)+li(3) pos(4)+li(2)+li(4)]);
