9d5b1542abec84fbcfd3d0d87c83e3f5e8fb2ba1
rmcdermo <randy.mcdermott@gmail.com>
2017-05-02 11:53:17 -0400

    Utilities: add get_folder_list.m

---
+++ Utilities/get_folder_list.m
@@ -0,0 +1,11 @@
+% return the directory folder list without hidden files or directories
+function folder_list = get_folder_list(indir)
+indir_list=dir(indir);
+folder_list={};
+jj=0;
+for j=1:length(indir_list)
+    if ~strcmp(indir_list(j).name(1),'.')
+        jj=jj+1;
+        folder_list{jj}=indir_list(j).name;
+    end
+end
