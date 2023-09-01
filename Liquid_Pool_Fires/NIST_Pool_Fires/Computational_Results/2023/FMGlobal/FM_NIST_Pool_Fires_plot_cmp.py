416a1ba4ad24ce6af92a338a6cd0c823c5b01cdd
Ning Ren <renn@renn-mac.corp.fmglobal.com>
2023-08-31 21:49:40 -0400

    Add the NIST pool fire case

---
+++ Liquid_Pool_Fires/NIST_Pool_Fires/Computational_Results/2023/FMGlobal/FM_NIST_Pool_Fires_plot_cmp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# McDermott
+# Feb 2021
+
+# first, make sure the macfp module directory is in your path
+# if not, uncomment the lines below and replace <path to macfp-db>
+# with the path (absolute or relative) to your macfp-db repository
+
+import sys
+# sys.path.append('<path to macfp-db>/macfp-db/Utilities/')
+sys.path.append('../../../../../../macfp-db/Utilities/')
+
+import macfp
+import importlib
+importlib.reload(macfp) # use for development (while making changes to macfp.py)
+import matplotlib.pyplot as plt
+
+macfp.dataplot(config_filename='FM_NIST_Pool_Fires_cmp_config.csv',
+               institute='FMGlobal',
+               revision='MaCFP-3, Tsukuba, 2023',
+               expdir='../../../Experimental_Data/',
+               cmpdir='./Output/',
+               pltdir='./Plots/',
+               close_figs=True,
+               verbose=True,
+               plot_list=['all'])
+
+# plt.show()
